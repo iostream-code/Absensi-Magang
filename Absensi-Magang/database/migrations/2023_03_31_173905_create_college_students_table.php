c9f63659808b870c472eacdd17bf35944e562e33
iostream-code <aryakmifjannah2603@gmail.com>
2023-04-01 01:03:53 +0700

    Create Database and Realtions

---
+++ Absensi-Magang/database/migrations/2023_03_31_173905_create_college_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('college_students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20)->required();
+            $table->string('status', 3)->required();
+            $table->string('ip_address', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('college_students');
+    }
+};
