a864088822b85924de96c22a99ea9d43d0858ccb
iostream-code <aryakmifjannah2603@gmail.com>
2023-04-04 23:16:08 +0700

    Test commit

--- Absensi-Magang/resources/views/layouts/error.blade.php
+++ Absensi-Magang/resources/views/layouts/error.blade.php
@@ -10,6 +10,11 @@
 
 <body>
     <h2>Ini adalah halaman error</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed officia voluptas expedita illo natus quis, ab quo
+        magnam sint, corporis, in unde mollitia exercitationem dicta vel! Rem perferendis id dignissimos doloribus sit
+        exercitationem, aliquam provident voluptatem rerum mollitia ratione voluptatibus.
+    </p>
 </body>
 
 </html>
