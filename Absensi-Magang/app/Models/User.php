36b785805abbd0478c112c23d0b71b609585fc67
iostream-code <aryakmifjannah2603@gmail.com>
2023-04-11 07:11:36 +0700

    Modify login controller

--- Absensi-Magang/app/Models/User.php
+++ Absensi-Magang/app/Models/User.php
@@ -2,14 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
-    protected $guarded = ['id'];
+    // protected $guarded = ['id'];
 
-    public function Presences()
-    {
-        return $this->hasMany(Presence::class);
-    }
+    // public function Presences()
+    // {
+    //     return $this->hasMany(Presence::class);
+    // }
+
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'username',
+        'status',
+        'role',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
