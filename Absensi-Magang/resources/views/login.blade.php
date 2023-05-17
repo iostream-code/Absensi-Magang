3e1d3f763d858ba7eb1a08cef656b8eb14e01820
iostream-code <aryakmifjannah2603@gmail.com>
2023-05-17 21:39:08 +0700

    adding some in login

--- Absensi-Magang/resources/views/login.blade.php
+++ Absensi-Magang/resources/views/login.blade.php
@@ -22,43 +22,46 @@
 </head>
 
 <body>
-    <section class="vh-100" style="background: #373B44;
+    <section class="vh-100"
+        style="background: #373B44;
     background: linear-gradient(90deg, #373B44 0%, #4286f4 75%);">
         <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                <div class="card-body p-5 text-center">
-      
-                  <h3 class="mb-5">Sign in</h3>
-                  <form action="postlogin" method="POST">
-                    @csrf
-                    <div class="form-outline mb-4">
-                        <label class="form-label" for="typeEmailX-2">Email</label>
-                        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" name="email"/>
-                      </div>
-          
-                      <div class="form-outline mb-4">
-                        <label class="form-label" for="typePasswordX-2">Password</label>
-                        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" name="password"/>
-                      </div>
-          
-                      <!-- Checkbox -->
-                      <div class="form-check d-flex justify-content-start mb-4">
-                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
-                        <label class="form-check-label" for="form1Example3"> Remember password </label>
-                      </div>
-          
-                      <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-                  </form>
-                  <div class="text-center mt-5 fs-lg-4 fs-sm-6">
-                    <p class="text-gray-600">Don't have an account? <a href="{{url('/register')}}" class="font-bold">Sign up</a>.</p>
-                    <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+
+                            <h3 class="mb-5">Sign in</h3>
+                            <form action="postlogin" method="POST">
+                                @csrf
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typeEmailX-2">Email</label>
+                                    <input type="email" id="typeEmailX-2" class="form-control form-control-lg"
+                                        name="email" />
+                                </div>
+
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typePasswordX-2">Password</label>
+                                    <input type="password" id="typePasswordX-2" class="form-control form-control-lg"
+                                        name="password" />
+                                </div>
+
+                                <!-- Checkbox -->
+                                <div class="form-check d-flex justify-content-start mb-4">
+                                    <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                                    <label class="form-check-label" for="form1Example3"> Remember password </label>
+                                </div>
+
+                                <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                            </form>
+                            <div class="text-center mt-5 fs-lg-4 fs-sm-6">
+                                <p class="text-gray-600">Don't have an account? <a href="{{ url('/register') }}"
+                                        class="font-bold">Sign up</a>.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
     </section>
 </body>
